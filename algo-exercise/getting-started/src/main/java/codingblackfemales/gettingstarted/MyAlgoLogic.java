b0266bfba741e9db0fb8fbf132ca6eecc4bca983
Poornima <Poornimapatil124@gmail.com>
2024-09-16 16:45:28 +0100

    assignment

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -1,10 +1,14 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
+import messages.order.Side;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +19,8 @@ public class MyAlgoLogic implements AlgoLogic {
     @Override
     public Action evaluate(SimpleAlgoState state) {
 
+        logger.info("[MYALGO] In Algo Logic....");
+
         var orderBookAsString = Util.orderBookToString(state);
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
@@ -24,7 +30,20 @@ public class MyAlgoLogic implements AlgoLogic {
          * Add your logic here....
          *
          */
+        final BidLevel nearTouch = state.getBidAt(0);
+        long quantity = nearTouch.quantity;
+        long price = nearTouch.price;
 
-        return NoAction.NoAction;
+        if (state.getChildOrders().size() < 4) {
+
+            logger.info("[MyALGO] Have:" + state.getChildOrders().size()
+                    + " children, want 4, joining my side of book with: " + quantity + " @ " + price);
+            return new CreateChildOrder(Side.BUY, quantity, price);
+
+        } else {
+            logger.info("[MyALGO] Have:" + state.getChildOrders().size() + " children, want 4, done.");
+            return NoAction.NoAction;
+        }
+
     }
 }
--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/AbstractAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/AbstractAlgoTest.java
@@ -18,7 +18,6 @@ import java.nio.ByteBuffer;
 
 public abstract class AbstractAlgoTest extends SequencerTestCase {
 
-
     protected AlgoContainer container;
 
     @Override
@@ -29,8 +28,9 @@ public abstract class AbstractAlgoTest extends SequencerTestCase {
         final RunTrigger runTrigger = new RunTrigger();
         final Actioner actioner = new Actioner(sequencer);
 
-        container = new AlgoContainer(new MarketDataService(runTrigger), new OrderService(runTrigger), runTrigger, actioner);
-        //set my algo logic
+        container = new AlgoContainer(new MarketDataService(runTrigger), new OrderService(runTrigger), runTrigger,
+                actioner);
+        // set my algo logic
         container.setLogic(createAlgoLogic());
 
         network.addConsumer(new LoggingConsumer());
@@ -43,8 +43,7 @@ public abstract class AbstractAlgoTest extends SequencerTestCase {
 
     public abstract AlgoLogic createAlgoLogic();
 
-
-    protected UnsafeBuffer createTick(){
+    protected UnsafeBuffer createTick() {
 
         final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
         final BookUpdateEncoder encoder = new BookUpdateEncoder();
@@ -52,10 +51,10 @@ public abstract class AbstractAlgoTest extends SequencerTestCase {
         final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
         final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
 
-        //write the encoded output to the direct buffer
+        // write the encoded output to the direct buffer
         encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
 
-        //set the fields to desired values
+        // set the fields to desired values
         encoder.venue(Venue.XLON);
         encoder.instrumentId(123L);
 
@@ -75,6 +74,37 @@ public abstract class AbstractAlgoTest extends SequencerTestCase {
         return directBuffer;
     }
 
+    protected UnsafeBuffer createTickmore() {
 
+        final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+        final BookUpdateEncoder encoder = new BookUpdateEncoder();
+
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        // write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+
+        // set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(124L);
+
+        encoder.askBookCount(4)
+                .next().price(100L).size(101L)
+                .next().price(110L).size(200L)
+                .next().price(115L).size(5000L)
+                .next().price(120L).size(7000L);
+
+        encoder.bidBookCount(4)
+                .next().price(98L).size(100L)
+                .next().price(95L).size(200L)
+                .next().price(91L).size(300L)
+                .next().price(90L).size(400L);
+
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+        encoder.source(Source.STREAM);
+
+        return directBuffer;
+    }
 
 }
