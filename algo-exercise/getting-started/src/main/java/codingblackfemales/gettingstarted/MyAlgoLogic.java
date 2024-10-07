b42dff00e97baf0c3de7b65c2c2aa3a2026b8339
Beth Austin <muchiribeth@yahoo.co.uk>
2024-10-07 23:01:15 +0100

    did initial work on MyAlgoLogic and MyAlgoTest classes. BUILD SUCCESS for all tests on MyAlgoTest class. need to do more work on the tests

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -1,15 +1,25 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.CancelChildOrder;
+import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.AskLevel;
+import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
+import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MyAlgoLogic implements AlgoLogic {
+// 8TH AND FINAL CODE - GOOD CODE
+// CODE TO CREATE AND CANCEL AN ORDER BASED ON THE SPREAD NARROWING AND EXPANDING
+// IF THE SPREAD NARROWS TO OR BELOW 5 POINTS CREATE 5 ORDERS FOR 200 SHARES PER ORDER,
+// IF THE SPREAD WIDENS TO OR ABOVE 10 POINTS, CANCEL ANY UNFILLED OR PARTIALLY FILLED ORDER
 
+public class MyAlgoLogic implements AlgoLogic {
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
 
     @Override
@@ -19,12 +29,167 @@ public class MyAlgoLogic implements AlgoLogic {
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
-        /********
-         *
-         * Add your logic here....
-         *
-         */
+        /** Algo **/
 
+        // Get the best bid and best ask levels     
+        BidLevel bestBid = state.getBidAt(0);
+        AskLevel bestAsk = state.getAskAt(0);
+
+        // Get the best bid and ask prices and their corresponding quantities
+        final long bestBidPrice = bestBid.getPrice();
+        final long bestAskPrice = bestAsk.getPrice();
+        final long bestBidQuantity = bestBid.getQuantity();
+        final long bestAskQuantity = bestAsk.getQuantity();
+        final long buyQuantity = 200L;
+
+        // Define a threshold for the spread (in price points)
+        long spread = Math.abs(bestAskPrice - bestBidPrice); // added 5/10/2024 to get absolute value of a number
+        long spreadThreshold = 5L;
+
+        // Log the bid-ask information
+        logger.info("[MYALGO] Best bid: " + bestBidQuantity + " @ " + bestBidPrice + " Best ask: " + bestAskQuantity + " @ " + bestAskPrice);
+
+        // returns all orders regardless of their status (ie active, cancelled, filled, modified, etc)
+        // var totalOrderCount = state.getChildOrders().size();    
+
+        // if (totalOrderCount > 20) { // original code            
+        //     return NoAction.NoAction;
+        // }
+    
+        
+        // Retrieve the list of active child orders
+        var activeChildOrders = state.getActiveChildOrders();
+
+
+        // TO CREATE CHILD ORDERS
+        // If there are less than 5 active child orders, and the spread has narrowed to the defined threshold,
+        // create a buy limit order at the best BID price and defined buy quantity
+
+        // if (activeChildOrders.isEmpty()) { // created 1
+        if (activeChildOrders.size() < 5) {
+
+            if (spread <= spreadThreshold) {
+                logger.info("[MYALGO] BUY CONDITIONS - Spread is " + spread + " points.");
+                logger.info("BUY CONDITIONS - [MYALGO] has " + activeChildOrders.size() + " active child orders. " +
+                            " Spread is below threshold. Creating a buy order for " + buyQuantity + " units @ " + bestBidPrice);
+                return new CreateChildOrder(Side.BUY, buyQuantity, bestBidPrice);
+            }
+            else {
+                // Log if the spread does not meet the threshold
+                logger.info("[MYALGO] BUY CONDITIONS - Spread is " + spread + " points.");
+                logger.info("[MYALGO] BUY CONDITIONS - Spread is above the buying threshold. No buy order created.");
+            }
+        }      
+        else {
+            // If there are already 5 active child orders, log it and take no action
+            logger.info("BUY CONDITIONS - [MYALGO] has " + activeChildOrders.size() + " active child orders. No new orders will be created.");
+        }             
+        
+
+        // CONDITIONS TO CANCEL A BUY ORDER        
+
+        long orderId;
+        Side orderSide;        
+        long orderQuantity;        
+        long orderPrice;
+        long orderFilledQuantity;
+        long unfilledQuantity;
+
+
+        // Define a price reversal threshold (in price points)            
+        long priceReversalThreshold = 7L;  // was 10L          
+
+        // amended code - now cancelling partially filled or unfilled orders
+        if (!activeChildOrders.isEmpty()) {                      
+
+            for (ChildOrder order : activeChildOrders) {
+                if (order.getSide() == Side.BUY) {
+                    orderId = order.getOrderId();
+                    orderSide = order.getSide();
+                    orderQuantity = order.getQuantity();                    
+                    orderPrice = order.getPrice();
+                    orderFilledQuantity = order.getFilledQuantity();
+
+                        // Check if the order is partially filled or not filled at all
+                        if (orderFilledQuantity < orderQuantity) {
+
+                                // Calculate the remaining unfilled quantity
+                                unfilledQuantity = orderQuantity - orderFilledQuantity;
+
+                                // Log the details of the unfilled or partially filled orders
+                                logger.info("[MYALGO] CANCEL CONDITIONS - Partially filled or Non filled order found. " +
+                                            " Order ID: " + orderId +
+                                            " Side: " + orderSide +
+                                            ", Ordered Qty: " + orderQuantity +
+                                            ", Price: " + orderPrice +                                            
+                                            ", Filled Qty: " + orderFilledQuantity  +
+                                            ", Unfilled Qty: " + unfilledQuantity);
+
+                                // Cancel the unfilled part of the order if the ASK price moves up by or above the defined threshold
+                                if (bestAskPrice >= (orderPrice + priceReversalThreshold)) {
+                                    logger.info("[MYALGO] CANCEL CONDITIONS - price reversal threshold is " + priceReversalThreshold + " points.");
+                                    logger.info("[MYALGO] CANCEL CONDITIONS - Ask price moved against buy order. " +
+                                                " Cancelling order ID: " + orderId +
+                                                ", Unfilled Qty: " + (unfilledQuantity));
+                                    return new CancelChildOrder(order);
+                                }
+                                else {
+                                    // If the ask price has not moved beyond the threshold, log that the order remains active
+                                    logger.info("[MYALGO] CANCEL CONDITIONS  - Ask price has not moved above threshold. Buy order to remain active");
+                                }
+                        }
+                        else {
+                            // Code to log if there are no unfilled quantities
+                            logger.info("[MYALGO] CANCEL CONDITIONS - No orders need cancelling. No action taken.");
+                            }
+                }                     
+                else {
+                    // if it was not a buy order, log this and take no action
+                    logger.info("[MYALGO] CANCEL CONDITIONS -  This was not a buy order. No action taken.");
+                }
+
+            }
+
+        }  
+        
+        // If no active child order is present, the method returns a NoAction object
         return NoAction.NoAction;
+    
     }
+
 }
+
+
+
+
+// // ORIGINAL CODE
+// package codingblackfemales.gettingstarted;
+
+// import codingblackfemales.action.Action;
+// import codingblackfemales.action.NoAction;
+// import codingblackfemales.algo.AlgoLogic;
+// import codingblackfemales.sotw.SimpleAlgoState;
+// import codingblackfemales.util.Util;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+
+// public class MyAlgoLogic implements AlgoLogic {
+
+//     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
+
+//     @Override
+//     public Action evaluate(SimpleAlgoState state) {
+
+//         var orderBookAsString = Util.orderBookToString(state);
+
+//         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
+
+//         /********
+//          *
+//          * Add your logic here....
+//          *
+//          */
+
+//         return NoAction.NoAction;
+//     }
+// }
--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -1,10 +1,17 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
-
-import static org.junit.Assert.assertEquals;
-
+import codingblackfemales.sotw.ChildOrder;
+import messages.marketdata.BookUpdateEncoder;
+import messages.marketdata.InstrumentStatus;
+import messages.marketdata.MessageHeaderEncoder;
+import messages.marketdata.Source;
+import messages.marketdata.Venue;
+import org.agrona.concurrent.UnsafeBuffer;
+import java.nio.ByteBuffer;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 
 /**
@@ -25,16 +32,218 @@ public class MyAlgoTest extends AbstractAlgoTest {
         return new MyAlgoLogic();
     }
 
+   protected UnsafeBuffer createTick2(){
+        final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+        final BookUpdateEncoder encoder = new BookUpdateEncoder();
+
+
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        //write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+
+        //set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(123L);
+        encoder.source(Source.STREAM);
+
+        encoder.askBookCount(4)
+                .next().price(99L).size(101L)
+                .next().price(110L).size(200L)
+                .next().price(115L).size(5000L)
+                .next().price(119L).size(5600L);        
+
+        encoder.bidBookCount(3)
+                .next().price(98L).size(100L)
+                .next().price(95L).size(200L)
+                .next().price(91L).size(300L);
+
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+
+        return directBuffer;
+    }
+
+    protected UnsafeBuffer createTick3(){
+
+        final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+        final BookUpdateEncoder encoder = new BookUpdateEncoder();
+
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        //write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+
+        //set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(123L);
+        encoder.source(Source.STREAM);
+
+        encoder.askBookCount(4)
+                .next().price(98L).size(501L)
+                .next().price(101L).size(200L)
+                .next().price(110L).size(5000L)
+                .next().price(119L).size(5600L);
+
+        encoder.bidBookCount(3)
+                .next().price(95L).size(100L)
+                .next().price(93L).size(200L)
+                .next().price(91L).size(300L);
+
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+
+        return directBuffer;
+    }
+
+    //CHECKING THE ALGO'S BEHAVIOUR AFTER 1ST TICK
 
     @Test
-    public void testDispatchThroughSequencer() throws Exception {
+    public void testDispatchThroughSequencerFirstTestTick1() throws Exception {
 
-        //create a sample market data tick....
+        //create a sample market data tick from first tick
         send(createTick());
 
-        //simple assert to check we had 3 orders created
-        assertEquals(container.getState().getChildOrders().size(), 3);
+            //assert to check we had 5 orders created
+            assertEquals(5, container.getState().getChildOrders().size());
+
+
+            //assert to check that we created a Buy order for 200 shares at 100 and no Sell orders
+            
+            for (ChildOrder childOrder : container.getState().getChildOrders()) {
+
+                assertEquals(messages.order.Side.BUY, childOrder.getSide());
+                assertNotEquals(messages.order.Side.SELL, childOrder.getSide());
+                assertEquals(200, childOrder.getQuantity());
+                assertEquals(100, childOrder.getPrice());
+
+                System.out.println("Order ID: " + childOrder.getOrderId() + 
+                                " | Side: " + childOrder.getSide() +            
+                                " | Price: " + childOrder.getPrice() +
+                                " | Ordered Qty: " + childOrder.getQuantity() +
+                                " | Filled Qty: " + childOrder.getFilledQuantity() +
+                                " | State of the order: " + childOrder.getState());
+            }
+
     }
+
+
+    //CHECKING THE ALGO'S BEHAVIOUR AFTER 2ND TICK
+
+    @Test
+    public void testDispatchThroughSequencerSecondTestTick2() throws Exception {
+
+       //create a sample market data tick from second tick
+       send(createTick2());
+
+           //assert to check we had 5 orders created
+           assertEquals(container.getState().getChildOrders().size(), 5);
+
+
+           //assert to check that we created a Buy order for 200 shares at 99 and no Sell orders
+           
+           for (ChildOrder childOrder : container.getState().getChildOrders()) {
+
+               assertEquals(childOrder.getSide(), messages.order.Side.BUY);
+               assertNotEquals(childOrder.getSide(), messages.order.Side.SELL);
+               assertEquals(childOrder.getQuantity(),  200);
+               assertEquals(childOrder.getPrice(),  99);
+               assertEquals(childOrder.getFilledQuantity(),  0);
+
+               System.out.println("Order ID: " + childOrder.getOrderId() + 
+                               " | Side: " + childOrder.getSide() +            
+                               " | Price: " + childOrder.getPrice() +
+                               " | Ordered Qty: " + childOrder.getQuantity() +
+                               " | Filled Qty: " + childOrder.getFilledQuantity() +
+                               " | State of the order: " + childOrder.getState());
+        }
+
+    }
+
+
+        
+    //check that our algo state was updated when the market data changed
+
+    @Test
+    public void testDispatchThroughSequencerThirdTestTick2() throws Exception {
+     
+        //create a sample market data tick from second tick
+        send(createTick2());
+
+        var state = container.getState();         
+
+        int totalOrdersCountTick2 = container.getState().getChildOrders().size(); // added 15/9/2024
+        int activeChildOrdersTick2 = container.getState().getActiveChildOrders().size();
+        int cancelledChildOrdersTick2 = totalOrdersCountTick2 - activeChildOrdersTick2;
+
+        long totalOrderedQuantityTick2 = state.getChildOrders().stream().map(ChildOrder::getQuantity).reduce (Long::sum).get();            
+        long filledQuantityTick2 = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
+        long unFilledQuantityTick2 = totalOrderedQuantityTick2 - filledQuantityTick2;
+
+        //Check total quantity ordered, filled quantity, unfilled quantity, cancelled order count etc....
+        assertEquals(1000, totalOrderedQuantityTick2);            
+        assertEquals(0, filledQuantityTick2);
+        assertEquals(1000, unFilledQuantityTick2);           
+        assertEquals(0, cancelledChildOrdersTick2);
+
+    }
+
+    //CHECKING THE ALGO'S BEHAVIOUR AFTER 3RD TICK
+
+    @Test
+    public void testDispatchThroughSequencerFourthTestTick3() throws Exception {
+
+        //create a sample market data tick from 3rd tick
+        send(createTick3());
+
+            //assert to check we had 5 orders created
+            assertEquals(container.getState().getChildOrders().size(), 5);        
+
+            //assert to check that we created a Buy order for 200 shares at 98 and no sell orders           
+            for (ChildOrder childOrder : container.getState().getChildOrders()) {
+
+                assertEquals(childOrder.getSide(), messages.order.Side.BUY);
+                assertNotEquals(childOrder.getSide(), messages.order.Side.SELL);
+                assertEquals(childOrder.getQuantity(),  200);
+                assertEquals(childOrder.getPrice(),  98);
+                assertEquals(childOrder.getFilledQuantity(),  0);
+
+                System.out.println("Order ID: " + childOrder.getOrderId() + 
+                                " | Side: " + childOrder.getSide() +            
+                                " | Price: " + childOrder.getPrice() +
+                                " | Ordered Qty: " + childOrder.getQuantity() +
+                                " | Filled Qty: " + childOrder.getFilledQuantity() +
+                                " | State of the order: " + childOrder.getState());
+            }
+
+    }
+
+
+
+    @Test
+    public void testDispatchThroughSequencerFifthTestTick3() throws Exception {
+        
+        //create a sample market data tick from 3rd tick
+        send(createTick3());
+        var state = container.getState();
+
+
+        int totalOrdersCountTick3 = container.getState().getChildOrders().size(); // added 15/9/2024
+        int activeChildOrdersTick3 = container.getState().getActiveChildOrders().size();
+        int cancelledChildOrdersTick3 = totalOrdersCountTick3 - activeChildOrdersTick3;
+
+        long totalOrderedQuantityTick3 = state.getChildOrders().stream().map(ChildOrder::getQuantity).reduce (Long::sum).get();            
+        long filledQuantityTick3 = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
+        long unFilledQuantityTick3 = totalOrderedQuantityTick3 - filledQuantityTick3;
+
+        //Check total quantity ordered, filled quantity, unfilled quantity, cancelled order count etc....
+        assertEquals(1000, totalOrderedQuantityTick3);            
+        assertEquals(0, filledQuantityTick3);
+        assertEquals(1000, unFilledQuantityTick3);           
+        assertEquals(0, cancelledChildOrdersTick3);
+
+    }   
+
 }
 
 
@@ -76,3 +285,4 @@ public class MyAlgoTest extends AbstractAlgoTest {
 // }
 
 // mvn test -pl :getting-started -DMyAlgoTest 
+// mvn -Dtest=MyAlgoTest test --projects algo-exercise/getting-started
