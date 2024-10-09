058f9f07955ee075ec566bd312651dd1e7f78c50
ayerose <reya-k@outlook.de>
2024-10-09 21:08:55 +0100

    prevent multiple order creations/cancellations and fix StackOverflowError

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -15,6 +15,9 @@ import org.slf4j.LoggerFactory;
 public class MyAlgoLogic implements AlgoLogic {
 
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
+    private boolean orderJustCancelled = false;  //  track if order was just canceled
+
+
 
     //create order if the price is below x value
     //cancel order if the price goes above x threshold
@@ -40,14 +43,14 @@ public class MyAlgoLogic implements AlgoLogic {
             var firstActiveOrder = activeOrders.stream().findFirst().get();
 
             // if ask price drops below target price, cancel old order
-            if (bestAskPrice < currentTargetPrice) {
-                logger.info("[ADDCANCELALGO] Market price has dropped below target. Cancelling order @: £" + currentTargetPrice);
-                // cancel existing order
+            if (bestAskPrice < currentTargetPrice && !orderJustCancelled) {
+                logger.info("[ADDCANCELALGO] Market price has dropped below target. Cancelling order @ £" + currentTargetPrice);
+                orderJustCancelled = true;  // set true to prevent re-creation of the order
                 return new CancelChildOrder(firstActiveOrder);
             }
         }
         // if no active order exist or previous one is cancelled, create new order
-        if (activeOrders.isEmpty()) {
+        if (activeOrders.isEmpty() && !orderJustCancelled) {
             if (bestAskPrice < currentTargetPrice) {
                 currentTargetPrice = bestAskPrice;
                 logger.info("[MYALGO] Creating new order @ new price: £" + currentTargetPrice);
@@ -60,6 +63,10 @@ public class MyAlgoLogic implements AlgoLogic {
             }
         }
 
+        // reset bool if no cancellations or creations happened
+        if (!activeOrders.isEmpty()) {
+            orderJustCancelled = false;
+        }
 
         return NoAction.NoAction;
     }
